b80a1b1114ddaba07a50af009af98f5aa5284184
louisvagner <43639349+louisvagner@users.noreply.github.com>
2018-10-11 11:09:58 +0100

    Update ex6.py
    changed values for answer for the age, the height and the weight

--- ex6.py
+++ ex6.py
@@ -1,19 +1,28 @@
 name = input("What is your name? : ")
 
-age = input("What is your age? ")
-if age <= 18:
-    print("Under 18")
-elif age >= 18 and age <= 25:
-    print("Between 18 and 25")
-elif age >= 25:
-    print("Over 25")
-"""Say if you are a)Under 18. b)Between 18 and 25. c)Over 25. : """
+age = input("What is your age? : ")
+if age <= "17":
+    print("--> Under 18")
+elif age >= "18" and age <= "21":
+    print("--> Between 18 and 21")
+elif age >= "22":
+    print("--> Over 21")
 
-height = input("How tall are you? ")
-"""Say if you are a)Under 160cm. b)Between 160cm and 180cm. c)Over 180cm : """
+height = input("How tall are you? (in cm) : ")
+if height <= "160":
+    print("--> Under 160 centimetre")
+elif height >= "160" and height <= "180":
+    print("--> Between 160 and 180 centimetre")
+elif height >= "180":
+    print("--> Over 180 centimetre")
 
-weight = input("How much do you weight? ")
-"""Say if you are a)Under 60kg. b)Between 60kg and 80kg. c)Over 80kg : """
+weight = input("How much do you weight? : ")
+if weight <= "60":
+    print("--> Under 60 kilos")
+elif weight >= "60" and weight <= "80":
+    print("--> Between 60 and 80 kilos")
+elif height >= "80":
+    print("--> Over 80 kilos")
 
 eye = input("What is the color of your eyes? : ")
 
