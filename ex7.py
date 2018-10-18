f08ac7b7b65165965db663f586f1ead051996e03
louisvagner <43639349+louisvagner@users.noreply.github.com>
2018-10-18 11:04:42 +0100

    Deleted stuff
    Delete useless informations, add actual code

--- ex7.py
+++ ex7.py
@@ -1,11 +1,25 @@
-"""total_cost = 120000
-portion_deposit = 0.20*total_cost
-current_savings = 1£
-r = current_savings*r/12
-annual_salary = 1450
-portion_saved = 0.22*1450
-monthly_salary = (annual_salary/12)"""
 
 annual_salary = input("What is your annual salary? : ")
 portion_saved = input("what percentage of your salary to you save? : ")
-total_cost = input("How much does the house you want to buy costs? : ")
+real_total_cost = input("How much does the house you want to buy costs? : ")
+
+portion_deposit = 0.20
+current_savings = 0
+annual_return = 0.04
+
+#reduce the total price of the house using the portion_deposit
+your_total_cost = int(real_total_cost) - int(real_total_cost)*portion_deposit
+
+annual_salary = int(annual_salary)
+annual_return = float(annual_return)
+
+monthly_salary = annual_salary/12
+current_savings= monthly_salary*portion_saved
+current_savings = current_savings*annual_return
+
+print(f"{current_savings}")
+
+months_needed = int(your_total_cost)/int(monthly_salary)
+months_needed = int(months_needed)
+
+print(f"You will need to save for {months_needed} months to buy the house.")
--- why.py
+++ why.py
@@ -1,1 +1,30 @@
-str = "I love chicken"
+month_counter = 0
+portion_deposit = 0.20
+current_savings = 0
+r = 0.04
+
+
+annual_salary = input ("Please enter your current annual salary: ")
+portion_saved = input ("Please enter how much you would like to be saved, as a decimal: ")
+total_cost = input ("Please enter the cost of your dream home: ")
+
+annual_salary = int(annual_salary)
+portion_saved = float(portion_saved)
+total_cost = float(total_cost)
+
+monthly_salary = annual_salary/12 
+
+while current_savings != total_cost:
+
+    current_savings = monthly_salary + (monthly_salary/r)
+    month_counter = month_counter + 1
+    
+    if current_savings == total_cost:
+        print (current_savings)
+        print (total_cost)
+
+
+    else:
+        print (current_savings)
+        print (total_cost)
+
