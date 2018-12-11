b8ab58997ff7bf1af7d74bddcc42ffd709664dc5
louisvagner <43639349+louisvagner@users.noreply.github.com>
2018-12-11 23:59:13 +0000

    First commit
    creation of the player commands, not yet defined

---
+++ Text Adventure Game/Personnage.py
@@ -0,0 +1,11 @@
+#find the position of the player
+get_position
+
+... = playerposition
+
+
+class Player:
+    def __init__(self, inventory, playerpositionX, playerpositionY):
+        self.inventory = inventory
+        self.playerpositionX, self.playerpositionY = playerposition
+
