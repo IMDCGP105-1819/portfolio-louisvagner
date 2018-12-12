afb36d540ce3ba12b2155775c405febbc7d437fb
louisvagner <43639349+louisvagner@users.noreply.github.com>
2018-12-12 00:02:30 +0000

    Less classes, more informations

--- Text Adventure Game/Salles.py
+++ Text Adventure Game/Salles.py
@@ -1,12 +1,17 @@
-class Barracks:
-    barraque = "You are in the barracks, you can see in the room different objects as an alarm clock, a uniform and an alarm button."
+class Room:
+    def __init__(self, description, items):
+        self.description = description
+        self.items = items
+
+barracks = Room("You are in the barracks, you can see in the room different objects as an alarm clock, a uniform and an alarm button.", ["alarm clock","uniforms","watch","alarm button"])
+barracks_list = ["uniform","watch"]
 #alarm clock
 #uniforms
 #watch
 #alarm button
 
-class Dining_Room:
-    salle_a_manger = "You are in the dinig room, you can see in the room different objects as some bottles, some plates, a jukebox, a fountain, a broom and an alarm button."
+dining_room = Room("You are in the dinig room, you can see in the room different objects as some bottles, some plates, a jukebox, a fountain, a broom and an alarm button.", ["bottles","plates","jukebox","fountain","broom","alarm button"])
+dining_list = ["bottles","broom"]
 #bottles
 #plates
 #jukebox
@@ -14,59 +19,59 @@ class Dining_Room:
 #broom
 #alarm button
 
-class Power_Plant:
-    centrale_electrique = "You are in the power plant, you can see in the room different objects as a notepad, a toolbox, a control panel and an extinguisher."
+power_plant = Room("You are in the power plant, you can see in the room different objects as a notepad, a toolbox, a control panel and an extinguisher.", ["notepad","toolbox","control panel","extinguisher"])
+power_list = ["notepad"]
 #notepad
 #toolbox
 #control panel
 #extinguisher
 
-class Water_Treatment_Plant:
-    centre_epuration = "You are in the water treatment plant, you can see in the room different objects as a notepad, atoolbox, an adhesif paper, a control panel and an extinguisher."
+water_treatment_plant= Room("You are in the water treatment plant, you can see in the room different objects as a notepad, atoolbox, an adhesif paper, a control panel and an extinguisher.", ["notepad","toolbox","adhesif","control panel","extinguisher"])
+water_list = ["notepad","adhesif"]
 #notepad
 #toolbox
 #adhesif
 #control panel
 #extinguisher
 
-class Hospital:
-    hopital = "You are in the hospital, you can see in the room different objects as beds, first aid kit, a lot of syringes."
+hospital = Room("You are in the hospital, you can see in the room different objects as beds, first aid kit, a lot of syringes.", ["beds","first aid kit","syringes"])
+hospital_list = []
 #beds
 #first aid kit
 #syringes
 
-class Storage_Room:
-    depot = "You are in the storage room, you can see in the room different objects as some boxes, junk and scrap, armors and guns and some weird rats."
+storage_room = Room("You are in the storage room, you can see in the room different objects as some boxes, junk and scrap, armors and guns and some weird rats.", ["boxes","junk boxes","armors","guns","rats"])
+storage_list = ["armors","guns"]
 #boxes
 #junks and scrap
 #armors
 #guns
 #rats
 
-class Science_Lab:
-    laboratoire = "You are in the science lab, you can see in the room different objects as chemicals, a huge microscope, some anti-radiation suits and anti-radiation care."
+science_lab = Room("You are in the science lab, you can see in the room different objects as chemicals, a huge microscope, some anti-radiation suits and anti-radiation care.", ["chemicals","microscope","anti-radiation suit","anti-radiation care"])
+science_list = ["syringes"]
+#syringes
 #chemicals
 #microscope
 #anti-radiation suit
 #anti-radiation care
 
-class Vault_Door:
-    entree = "You are in the vault door, you can see in the room a lever."
+vault_door = Room("You are in the vault door, you can see in the room a lever.", ["lever"])
+vault_list = ["lever"]
 #lever
 
-class Armory:
-    armurerie = "You are in the armory, you can see in the room different objects as gun parts, bullets and machines."
+armory = Room("You are in the armory, you can see in the room different objects as gun parts, bullets and machines.", ["gun parts","bullets","machines"])
+armory_list = []
 #gun parts
 #bullets
 #machines
 
-class Weight_Room:
-    salle_de_sport = "You are in the weight room, you can see in the room different objects as weights, ropes and a work bench."
+weight_room = Room("You are in the weight room, you can see in the room different objects as weights, ropes and a work bench.", ["weights","ropes","work bench"])
+waight_list = []
 #weights
 #ropes
 #work bench
 
-class Elevator:
-    ascenseur = "You are in the elevator, you can see on the wall level buttons and the shelter's map."
+elevator = Room("You are in the elevator, you can see on the wall level buttons and the shelter's map.", ["level button","shelter map"])
 #level buttons
 #shelter map
